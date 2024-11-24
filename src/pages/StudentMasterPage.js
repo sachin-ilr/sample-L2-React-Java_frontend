50f37641464a428cfed74c139e59a0bf0781c5d9
sachin ilr <sachu.ilrad@gmail.com>
2024-11-24 09:42:26 +0530

    correcting the tables imports

--- src/pages/StudentMasterPage.js
+++ src/pages/StudentMasterPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Table from "../components/Table";
+import Table from "../components/table";
 import { fetchData, postData, deleteData } from "../service/api";
 
 const StudentMasterPage = () => {
